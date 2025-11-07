a2b682a7c1f80e62ad7b5c5b30573aeddef5325d
alialaa <hello@alialaa.com>
2025-11-07 11:22:15 +0200

    Lecture Code For remove-fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 export default async function Home() {
   console.log("Home Component Ran");
-  const randomWord = await fetch(
-    `https://random-word-api.herokuapp.com/word`
-  ).then((r) => r.json());
+
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-4xl font-bold">{randomWord}</h1>
+      <h1 className="text-4xl font-bold">Hello</h1>
     </div>
   );
 }
