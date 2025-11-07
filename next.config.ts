ec985a8f1224d067ba87b3e85a454213daa7c5c1
Ali Alaa <hello@alialaa.com>
2025-11-07 10:51:31 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   cacheComponents: true,
-   experimental: {
-    staleTimes: {
-      dynamic: 30
-    }
-   }
 };
 
 export default nextConfig;
